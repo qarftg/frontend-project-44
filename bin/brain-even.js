78e977825b767f8f3c7ac993fd3ccff5523b7c25
qarftg <andrey.sizov.03z@gmail.com>
2024-02-07 13:46:33 +0700

    pips

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,26 @@
 import user from '../src/cli.js'
 import getRandomInt from '../src/utilities.js';
-
+import { name } from '../src/cli.js';
+import readlineSync from 'readline-sync';
 const firstNumber = getRandomInt();
 const brainEven = () => {
     user()
     console.log('answer "yes" if the number is even, otherwise answer "no".')
     console.log(`Question: ${firstNumber}`)
+    let i = ['yes', 'no']
+    i = readlineSync.question('your answer:')
+    if (i === 'yes') {
+        i = 0
+    }
+    if (i === 'no') {
+        i = 1
+    }
+    if (i === firstNumber % 2) {
+        console.log('Correct!');
+    }else{
+        console.log(`${i} is wrong answer Let's try again, ${name}`);
+    }
 }
+
 brainEven()
 export default brainEven;
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
+let name;
 const user = () => {
     console.log("Welcome to the brain games")
-    const name = readlineSync.question('May I have your name? ');
+    name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
 }
-
-
+export {name}
 export default user;
