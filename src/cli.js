f7854a9800521c32f934d3234751f778a2e6282a
qarftg <andrey.sizov.03z@gmail.com>
2024-02-26 09:36:15 +0700

    bulba

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 let name;
 const user = () => {
-    console.log("Welcome to the brain games")
+    console.log("Welcome to the Brain Games!")
     name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
 }
