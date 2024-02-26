69c3b684313cbc9e9586c93823addf0f7116b901
qarftg <andrey.sizov.03z@gmail.com>
2024-02-26 12:43:09 +0700

    try fix bugs #3

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import name from '../src/cli.js';
+import { name } from '../src/cli.js';
 import user from '../src/cli.js';
 user();
 
