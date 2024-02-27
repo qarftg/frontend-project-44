6910601375029c0444436d274d2514987e851ebe
qarftg <andrey.sizov.03z@gmail.com>
2024-02-27 11:27:01 +0700

    buble3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import user from '../src/cli.js';
-    
+
 user();
--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,7 @@ const user = () => {
   console.log('Welcome to the Brain Games!');
   name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  
+
   return name;
 };
 
